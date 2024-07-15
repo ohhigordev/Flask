b903139d1cbce02bd38578129f67d675500c3b39
ohhigordev <higor.nascimento0704@gmail.com>
2024-07-15 11:04:26 -0300

    Projeto_musica

--- Projeto_musica/musica.py
+++ Projeto_musica/musica.py
@@ -1,12 +1,26 @@
 # Importando o flask para o projeto
 from flask import Flask,render_template
 
+# Vamos criar uma classe em python:
+class Musica:
+    def __init__(self, nome, cantorBandaGrupo, genero): # Aqui definimos os parametros!!!
+        self.nome = nome # Isso aqui são os atributos.
+        self.cantorBanda = cantorBandaGrupo
+        self.genero = genero
+
+
+
+
 app = Flask(__name__)
 
 @app.route('/musicas')
 def listarMusicas():
 
-    lista = ['Lobo - Guará','Terror da Previdencia','Vai e Chora']
+    musica01 = Musica('Temporal','Hungria', 'Hip-Hop')
+    musica02 = Musica('Papai Banca','MC Ryan SP', 'Funk')
+    musica03 = Musica('Camisa 10', 'Turma do Pagode', 'Pagode')
+
+    lista = [musica01, musica02, musica03]
 
 
     return render_template('lista_musicas.html', 
